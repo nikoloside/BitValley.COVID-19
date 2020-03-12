90df0a67c04f8e05cfc05875e9846081a89851d0
趙 天宇 <iamcliffzhao@gmail.com>
2020-03-12 14:51:06 +0900

    feat: implement infect region graph of desktop and mobile

---
+++ src/components/InfectRegionGraphDesktop.vue
@@ -0,0 +1,203 @@
+<template>
+  <div id="InfectRegionGraphDesktop">
+    <div class="unit-group">
+      <div class="unit" v-for="unit in units" v-bind:key="unit">
+        {{ unit }}人
+      </div>
+    </div>
+
+    <div class="graph-wrapper">
+      <div
+        class="unit-line"
+        v-for="bottom in lineBottoms"
+        v-bind:key="bottom"
+        v-bind:style="{ bottom }"
+      />
+
+      <div class="region-data-group">
+        <div
+          class="region-data"
+          v-for="regionData in regionDatas"
+          v-bind:key="regionData.region"
+          v-bind:style="{ width: `${100 / regionDatas.length}%` }"
+        >
+          <div
+            class="region-column"
+            v-bind:style="{ height: `${regionData.count / units[0] * 100}%` }"
+          ></div>
+        </div>
+      </div>
+    </div>
+
+    <div/>
+
+    <div class="region-label-group">
+      <div class="region-label"
+        v-for="regionData in regionDatas"
+        v-bind:key="regionData.region"
+        v-bind:style="{ width: `${100 / regionDatas.length}%` }"
+      >
+        {{regionData.region}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfectRegionGraphDesktop',
+  data() {
+    return {
+      unitCount: 7,
+      regionDatas: [
+        {
+          region: '北海道',
+          count: 110,
+        },
+        {
+          region: '愛知県',
+          count: 96,
+        },
+        {
+          region: '東京都',
+          count: 62,
+        },
+        {
+          region: '大阪府',
+          count: 59,
+        },
+        {
+          region: '神奈川県',
+          count: 35,
+        },
+        {
+          region: 'その他１',
+          count: 35,
+        },
+        {
+          region: 'その他２',
+          count: 35,
+        },
+        {
+          region: 'その他３',
+          count: 35,
+        },
+        {
+          region: 'その他４',
+          count: 35,
+        },
+        {
+          region: 'その他５',
+          count: 35,
+        },
+        {
+          region: 'その他６',
+          count: 35,
+        },
+        {
+          region: 'その他７',
+          count: 35,
+        },
+        {
+          region: 'その他８',
+          count: 20,
+        },
+        {
+          region: 'その他９',
+          count: 10,
+        },
+        {
+          region: '中国居住者',
+          count: 3,
+        },
+      ],
+    };
+  },
+  computed: {
+    step() {
+      const { regionDatas, unitCount } = this;
+      const counts = regionDatas.map(({ count }) => count);
+      const maxCount = Math.max(...counts);
+
+      return Math.ceil(maxCount / (unitCount - 1) / 10) * 10;
+    },
+    lineBottoms() {
+      const { unitCount } = this;
+      // eslint-disable-next-line no-mixed-operators
+      return [...Array(unitCount - 1).keys()].map(i => `${i * 100 / (unitCount - 1)}%`);
+    },
+    units() {
+      const { unitCount, step } = this;
+      return [...Array(unitCount).keys()].map(i => i * step).reverse();
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "@/commons/_variables.scss";
+
+#InfectRegionGraphDesktop {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: auto auto;
+  gap: 4px 8px;
+  margin: 24px;
+
+  .unit-group {
+    text-align: left;
+    font-weight: bold;
+    font-size: 10px;
+    color: $color-gray;
+  }
+
+  .graph-wrapper {
+    position: relative;
+  }
+
+  .unit-line {
+    height: 1px;
+    background-color: $color-palegray;
+    position: absolute;
+    right: 0;
+    left: 0;
+  }
+
+  .region-data-group {
+    position: absolute;
+    display: flex;
+    top: 0;
+    right: 4px;
+    bottom: 0;
+    left: 4px;
+  }
+
+  .region-data {
+    position: relative;
+  }
+
+  .region-column {
+    position: absolute;
+    left: 4px;
+    right: 4px;
+    bottom: 0;
+    background-color: $color-navy;
+    border-radius: 4px 4px 0px 0px;
+  }
+
+  .region-label-group {
+    padding: 0 4px;
+    display: flex;
+  }
+
+  .region-label {
+    font-weight: bold;
+    font-size: 10px;
+    color: $color-gray;
+    text-align: center;
+    padding: 0 4px;
+    line-height: 1.1;
+  }
+}
+</style>
